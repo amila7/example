6d923e38a52f3e2569b869bcae1071073fc1b1b4
amila7 <dorape1017@gmail.com>
2024-08-15 18:54:53 +0900

    today level

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class job extends Model {
+    use HasFactory;
 
     protected $table = 'job_listings';
 
     protected $fillable =['title', 'salary'];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
 
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -42,4 +42,12 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    // public function admin(): static
+    // {
+    //     return $this->state(fn (array $attributes) => [
+    //         'admin'=> true,
+    //     ]);
+    // }
+    // User ::factory()->admin()->create()
 }
